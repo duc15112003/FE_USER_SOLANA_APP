8064c473bf7d62d4de2e11fea91201f5118d658e
thesonofwind99 <dunhan999@gmail.com>
2024-07-12 21:37:51 +0700

    ref #1 register form with validate

--- app/src/component/route/index.js
+++ app/src/component/route/index.js
@@ -1,6 +1,8 @@
 import Home from "../ingredient/Home/index";
+import Register from "../ingredient/Register/index";
 const publicRoutes = [
     { path: '/', component: Home },
+    { path: '/user/register', component: Register},
 ];
 
 export { publicRoutes };
