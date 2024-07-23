213e879eb43ef4484cf6fc646f61303e2f1bc66a
thesonofwind99 <dunhan999@gmail.com>
2024-07-23 14:51:31 +0700

    fix conflict

--- app/src/component/route/index.js
+++ app/src/component/route/index.js
@@ -3,8 +3,11 @@ import Login from "../ingredient/Login/index";
 
 import IdeaUser from "../ingredient/IdeaUser";
 import IdeaDetails from "../ingredient/IdeaDetails";
+import FogetPassword from "../ingredient/FogetPassword";
 const publicRoutes = [
     { path: '/', component: Home },
+    { path: '/user/login', component: Login},
+    { path: '/user/forgotpassword', component: FogetPassword},
     { path: '/user/login', component: Login},
     {path: '/ideas', component: IdeaUser},
     {path: '/ideas/details', component: IdeaDetails}
